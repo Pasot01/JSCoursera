70d8f2d42c2a4bf36de41da63b7da8974f0e6cfd
Pasot01 <contactmyphone14@gmail.com>
2024-03-25 06:59:07 -0400

    next function and dom

--- jscoursetesting/calculateArea/calculate_Area.js
+++ jscoursetesting/calculateArea/calculate_Area.js
@@ -7,3 +7,15 @@ function calculateArea() {
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let grocery_1;
+let grocery_2;
+let grocery_3;
+
+function groceryTracker() {
+    grocery_1 = parseFloat(document.getElementById('grocery_1').value);
+    grocery_2 = parseFloat(document.getElementById('grocery_2').value);
+    grocery_3 = parseFloat(document.getElementById('grocery_3').value);
+    let calculateTotalAmount = grocery_1 + grocery_2 + grocery_3;
+    document.getElementById('totalAmount').innerText = `The total amount is: ${calculateTotalAmount}`;
+}
