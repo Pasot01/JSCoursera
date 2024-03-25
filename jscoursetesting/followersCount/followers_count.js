80523fdd7d3228a92d64f4610153ec1c6deb371a
Pasot01 <contactmyphone14@gmail.com>
2024-03-25 07:37:09 -0400

    next functions closure and holsting

--- jscoursetesting/followersCount/followers_count.js
+++ jscoursetesting/followersCount/followers_count.js
@@ -6,15 +6,24 @@ function increaseCount() {
     checkCountValue(); // Check count value and display messages
 }
 
+function resetCount() {
+    count--; // Increment the count by 1
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and display messages
+}
+
 function checkCountValue() {
     if (count === 10) {
       alert("Your Instagram post gained 10 followers! Congratulations!");
     } else if (count === 20) {
       alert("Your Instagram post gained 20 followers! Keep it up!");
+    } else if (count === 0) {
+      alert("the Followers count has been reset!");
     }
 }
 
 function displayCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 }
+
 
